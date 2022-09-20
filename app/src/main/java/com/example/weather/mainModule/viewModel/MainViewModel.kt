d85cda8e9f80b114716fe6cbdfd84fcae76eec07
Edgar Reyes Gonzalez <gzzenit@gmail.com>
2022-09-20 15:39:29 -0500

    ViewModel

---
+++ app/src/main/java/com/example/weather/mainModule/viewModel/MainViewModel.kt
@@ -0,0 +1,40 @@
+package com.example.weather.mainModule.viewModel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.weather.R
+import com.example.weather.common.entities.WeatherForecastEntity
+import com.example.weather.mainModule.model.MainRepository
+import kotlinx.coroutines.launch
+
+class MainViewModel: ViewModel() {
+    private val repository = MainRepository()
+
+    private val result = MutableLiveData<WeatherForecastEntity>()
+    fun getResult(): LiveData<WeatherForecastEntity> = result
+
+    private val snackbarMsg = MutableLiveData<Int>()
+    fun getSnackbarMsg() = snackbarMsg
+
+    private val loaded = MutableLiveData<Boolean>()
+    fun isLoaded() = loaded
+
+    suspend fun getWeatherAddForecast(lat: Double, lon: Double, appId: String, units: String, lang: String){
+        viewModelScope.launch {
+            try {
+                loaded.value = false
+                val resultServer = repository.getWeatherAndForecast(lat, lon, appId, units, lang)
+                result.value = resultServer
+            }
+            catch (e: Exception){
+                snackbarMsg.value = R.string.main_error_server
+            }
+            finally {
+                loaded.value = true
+            }
+        }
+    }
+
+}
