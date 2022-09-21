696f2faf2f2031f954b778ad9192725d29c67a8d
Edgar Reyes Gonzalez <gzzenit@gmail.com>
2022-09-21 08:56:45 -0500

    BindingAdapters

--- app/src/main/java/com/example/weather/mainModule/viewModel/MainViewModel.kt
+++ app/src/main/java/com/example/weather/mainModule/viewModel/MainViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.weather.mainModule.viewModel
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
